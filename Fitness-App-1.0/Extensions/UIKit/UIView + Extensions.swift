1bcb4b9929519800e90deeeb70d10fee436baf3e
Vadim <91338647+pticagamaun@users.noreply.github.com>
2023-04-04 16:25:40 +0300

    Refactoring func addView

--- Fitness-App-1.0/Extensions/UIKit/UIView + Extensions.swift
+++ Fitness-App-1.0/Extensions/UIKit/UIView + Extensions.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 extension UIView {
-    
     func addShadowOnView() {
-        
         self.layer.shadowColor = UIColor.gray.cgColor
         self.layer.shadowOffset = CGSize(width: 0, height: 3)
         self.layer.shadowRadius = 1
         self.layer.shadowOpacity = 0.7
     }
     
+    
+    func addView(_ view: UIView) {
+        self.addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+    }
 }
