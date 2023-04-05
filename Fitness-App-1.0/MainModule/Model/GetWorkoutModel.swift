e71ba30d2b0cd9fea0ab2218f1b29fd229fd7701
Vadim <91338647+pticagamaun@users.noreply.github.com>
2023-04-05 12:26:32 +0300

    Add model for conform calendar model

--- Fitness-App-1.0/MainModule/Model/GetWorkoutModel.swift
+++ Fitness-App-1.0/MainModule/Model/GetWorkoutModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct GetWorkoutModel {
+    var workoutArray = [WorkoutModel]()
+    mutating func getWorkouts(date: Date) {
+        let weekday = date.getWeekdayNumber()
+        let startDay = date.dateEndStart().0
+        let endDay = date.dateEndStart().1
+        let predicateRepeat = NSPredicate(format: "workoutNumberOfDay = \(weekday) AND workoutRepeat = true")
+        let predicateUnRepeat = NSPredicate(format: "workoutRepeat = false AND workoutDate BETWEEN %@", [startDay, endDay])
+        let compoundPredicate = NSCompoundPredicate(type: .or, subpredicates: [predicateRepeat, predicateUnRepeat])
+        let resultsArray = RealmManager.shared.getWorkoutModelResults()
+        let filtredArray = resultsArray.filter(compoundPredicate).sorted(byKeyPath: "workoutName")
+        workoutArray = filtredArray.map{$0}
+    }
+}
